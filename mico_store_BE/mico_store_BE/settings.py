830bf4120135ca8f8bbb53c8e03c35ad6af700b7
michealeeee <mke00007@gmail.com>
2026-02-02 05:05:59 +0000

    mico BE

--- mico_store_BE/mico_store_BE/settings.py
+++ mico_store_BE/mico_store_BE/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mico_store_api_v1',
+    'mensah',
 ]
 
 MIDDLEWARE = [
--- mico_store_BE/mico_store_api_v1/models.py
+++ mico_store_BE/mico_store_api_v1/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from mongoengine import Document, StringField,IntField
 
 # Create your models here.
+class User(Document):
+    name = StringField
+    age = IntField
+    email = StringField
+    
+    
