263bf1a43681c9b3f34c4065ede5b83863cff282
michealeeee <mke00007@gmail.com>
2026-02-12 05:54:40 +0000

    host

--- mico_store_BE/mensah/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from mongoengine import Document, StringField
-
-# Create your models here.
-class School(Document):
-    name = StringField()
-    address = StringField()
-    country = StringField()
--- mico_store_BE/mico_store_api_v1/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import User
-from rest_framework_mongoengine import serializers
-
-class UserSerializer(serializers.DocumentSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
