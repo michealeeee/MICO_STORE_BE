aa05fadf6ff36feb6a4d10453f241e8f5f3ad508
michealeeee <mke00007@gmail.com>
2026-03-20 04:59:29 +0000

    user modified

--- mico_store_api_v1/models.py
+++ mico_store_api_v1/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from mongoengine import Document, StringField,IntField
+from mongoengine import Document, StringField,IntField,EmailField,DateField
 
 # Create your models here.
 class User(Document):
     name = StringField()
-    age = IntField()
-    email = StringField()
-    
-    
+    gender_ = StringField()
+    email = EmailField()
+    tel = StringField()
+    password = StringField()
+    DOR = DateField()
