a02cb6b9388ae90e5d3f7525045ac555aaa8b652
michealeeee <mke00007@gmail.com>
2026-03-30 04:31:22 +0000

    new changes

--- mico_store_BE/settings.py
+++ mico_store_BE/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 
 ]
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173/",
+    "http://localhost:5173",
     "http://localhost:8000",  # If testing locally with a frontend
 ]
 
